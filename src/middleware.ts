215334d31bc44b89ec9164760c0d1ecebdcb5eb6
omprakash <kanuparewa371@gmail.com>
2024-05-03 01:49:13 +0530

    first commit

---
+++ src/middleware.ts
@@ -0,0 +1,11 @@
+import { clerkMiddleware } from "@clerk/nextjs/server";
+ 
+export default clerkMiddleware();
+ 
+export const config = {
+  matcher: [
+    '/',
+    '/sign-in',
+    '/sign-up'
+  ],
+};
